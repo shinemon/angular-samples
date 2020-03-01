32201a072a49a7c1342e52e750be5bd529510fbb
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-03-01 23:10:22 +0000

    new component

--- my-sample-application/src/app/app.module.ts
+++ my-sample-application/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuPageComponent } from './menu-page/menu-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuPageComponent
   ],
   imports: [
     BrowserModule,
