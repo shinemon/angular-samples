fbb08ad249657a0d81ca255e0ebaeb1281eda2df
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-03-04 23:48:15 +0000

    Hero service additional updates

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit}from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 @Component({
 selector: 'app-heroes',
@@ -14,14 +15,18 @@ id: 1,
 name: 'Windstorm'
 };
 
-heroes = HEROES;
+heroes: Hero[];
 selectedHero: Hero;
 
-constructor() { }
+constructor(private heroService: HeroService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+this.getHeroes();
   }
 
+getHeroes(): void{
+this.heroes = this.heroService.getHeroes();
+}
 
 onSelect(hero: Hero): void {
 this.selectedHero = hero;
