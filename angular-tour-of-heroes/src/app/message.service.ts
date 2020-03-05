346b1efcbe3af82b0fb0d894fbe6b1ff0f8b90b6
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-03-05 23:36:26 +0000

    message service inject into hero service

--- angular-tour-of-heroes/src/app/message.service.ts
+++ angular-tour-of-heroes/src/app/message.service.ts
@@ -5,5 +5,14 @@ import { Injectable } from '@angular/core';
 })
 export class MessageService {
 
+messages: string[] = [];
+
+add(message: string){
+this.messages.push(message);
+}
+
+clear() {
+this.messages =[];
+}
   constructor() { }
 }
