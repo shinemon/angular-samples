44fde5a58258bd692c6d80add1f5288a7645a72f
mailshine <mailshine@gmail.com>
2020-03-11 23:21:47 +0000

    my workflow springboot and angular project start

---
+++ my-workflow/src/main/java/com/mailshine/my/workflow/MyWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.mailshine.my.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWorkflowApplication.class, args);
+	}
+
+}
---
+++ my-workflow/src/test/java/com/mailshine/my/workflow/MyWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mailshine.my.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
