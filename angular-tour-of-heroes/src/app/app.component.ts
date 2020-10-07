ff7f6e30ebb9c92da88f7a23417d8a9da657d19e
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-10-07 00:17:15 -0400

    session storage cypto-js examples

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import{Component}from'@angular/core';
+import {AES }from 'crypto-js';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+title = 'Tour of Heroes';
+
 }
--- angular-tour-of-heroes/src/app/messages/messages.component.ts
+++ angular-tour-of-heroes/src/app/messages/messages.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { MessageService } from '../message.service';
+import{Component, OnInit}from '@angular/core';
+import {MessageService}from '../message.service';
+import {AES, enc}from 'crypto-js';
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+selector: 'app-messages',
+templateUrl: './messages.component.html',
+styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
 
 constructor(public messageService: MessageService) {}
 
   ngOnInit() {
+
+    const envryptedString = AES.encrypt('Hello All','MYKEY4DEMO');
+    sessionStorage.setItem('encryptedHello', envryptedString.toString());
+
+    const decrypted = AES.decrypt(sessionStorage.getItem('encryptedHello'), 'MYKEY4DEMO');
+    const decryptedString = decrypted.toString(enc.Utf8);
+    sessionStorage.setItem('decryptedHello', decryptedString);
+
+    let obj = {"studentName":"John Smith","studentId":"CSC001"};
+    const envryptedObject = AES.encrypt(JSON.stringify(obj),'MYKEY4DEMO');
+    sessionStorage.setItem('envryptedObject', envryptedObject.toString());
+
+    const decrypted2 = AES.decrypt(sessionStorage.getItem('envryptedObject'), 'MYKEY4DEMO');
+    const decryptedObject = decrypted2.toString(enc.Utf8);
+    sessionStorage.setItem('decryptedObject', decryptedObject);
+
   }
 
 }
